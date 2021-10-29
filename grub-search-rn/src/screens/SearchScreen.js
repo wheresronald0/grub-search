c4906b84d2a23b9c64c2e441ed7f1606bd8b5e05
“Ron <wheresronald0@icloud.com>
2021-10-29 10:41:35 -0400

    minor boier plate

---
+++ grub-search-rn/src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  <View>
+    <Text style={styles.title}>Search Screen</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 22,
+  },
+});
+export default SearchScreen;
